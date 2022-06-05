7390335f850a21c2dc19b4cef9abc243d8afec41
ARJUN <100413585+AR-JUN-G@users.noreply.github.com>
2022-06-05 16:31:11 +0530

    Update lcd.c

--- lcd.c
+++ lcd.c
@@ -1,9 +1,11 @@
 #include<avr/io.h>
 #include<util/delay.h>
 
+#define LCDDDR DDRA
+#define CONTROLDDR DDRB
 #define lcdPort PORTA
 #define controlPort PORTB
-#define lcdPin PINA
+//#define lcdPin PINA
 #define EN PB2
 #define RW PB1
 #define RS PB0
@@ -13,34 +15,42 @@
 void enablePulse()
 {
     controlPort |=(1<<EN);
-    _delay_ms(10);
+    _delay_us(2);
     controlPort &=~(1<<EN);
 }
 
 
 
-void lcdWriteCommand(unsigned char cmd)
+void lcdWriteCommand(uint8_t cmd)
 {
     lcdPort =(cmd);
     controlPort &=~(1<<RW);
     controlPort &=~(1<<RS);
     enablePulse();  ////give high to low pulse
+    if(cmd == 0x01 || cmd ==0x02)
+    {
+        _delay_us(1530);
+    }
+    else{
+        _delay_us(50);
+    }
 }
 
-void lcdWriteData(unsigned char data)
+void lcdWriteData(uint8_t data)
 {
     lcdPort =(data);
     controlPort &=~(1<<RW);
     controlPort |=(1<<RS);
     enablePulse();  ////give high to low pulse
+    _delay_us(50);
 }
 
-void lcdWriteChar(unsigned char ch)
+void lcdWriteChar(const char ch)
 {
     lcdWriteData(ch);
 }
 
-void lcdWriteString(unsigned char *str)
+void lcdWriteString(const char *str) ///const 
 {
     while(*str)
     {
@@ -54,41 +64,45 @@ void lcdInit()
 
     //8bit mode
     lcdWriteCommand(0x38);
-    _delay_ms(1);
 
     ///clear Display
     lcdWriteCommand(0x01);
-    _delay_ms(1);
 
     ///return home
     lcdWriteCommand(0x02);
-    _delay_ms(1);
 
     ///make increment in the cursor 
-    lcdWriteCommand(0x06);
-    _delay_ms(1);
+    //lcdWriteCommand(0x06);
 
-    lcdWriteCommand(0X80);
-    _delay_ms(1);
+    //lcdWriteCommand(0X80);
     
 }
+
+void lcdGoto(uint8_t byte,uint8_t line)
+{
+    uint8_t command =(1<<7);
+    if(line==1)
+    {
+        byte+=40;
+    }
+    command +=byte;
+    lcdWriteCommand(command);
+}
+
+
 int main()
 {
-    
-    DDRA |= (1<<PA0);
-    DDRA |= (1<<PA1);
-    DDRA |= (1<<PA2);
-    DDRA |= (1<<PA3);
-    DDRA |= (1<<PA4);
-    DDRA |= (1<<PA5);
-    DDRA |= (1<<PA6);
-    DDRA |= (1<<PA7);
+    _delay_ms(100);
 
-    DDRB |= (1<<RW);
-    DDRB |= (1<<EN);
-    DDRB |= (1<<RS);
+    LCDDDR =0xFF;
+    lcdPort=0X00;
+    
+    CONTROLDDR |=(1<<EN) | (1<<RW) | (1<<RS);
+    
     lcdInit();
+    lcdGoto(0,1);
     lcdWriteString("Hello ALL");
-    lcdWriteChar("A");
+
+    //lcdWriteChar('A');
 
 }
